312c29581fa9a53b319c3edce6f142c5a7484004
WebPrivada <transportresargote@gmail.com>
2025-07-03 15:30:15 -0600

    Update STRUCTURES.go

--- db/STRUCTURES/STRUCTURES.go
+++ db/STRUCTURES/STRUCTURES.go
@@ -1,18 +1,5 @@
 package STRUCTURES
 
-/*
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct {
-    char* json;
-    int is_error;    // 1 si es error, 0 si es éxito
-    int is_empty;    // 1 si está vacío, 0 si tiene datos
-} SQLResult;
-*/
-import "C"
-
-// Estructuras para respuestas JSON
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
@@ -26,6 +13,3 @@ type InternalResult struct {
 	Is_error int
 	Is_empty int
 }
-
-
-
