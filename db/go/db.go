fff84af4918ed4b19b07cfcdb6497975cc684b83
WebPrivada <transportresargote@gmail.com>
2025-07-03 22:24:15 -0600

    Update db.go

--- db/go/db.go
+++ db/go/db.go
@@ -1,26 +1,26 @@
 package db
 
 import (
-    "encoding/base64"
-    "encoding/json"
-    "fmt"
-    "strconv"
-    "strings"
-    STRC "github.com/IngenieroRicardo/db/STRUCTURES"
-    LDB "github.com/IngenieroRicardo/db/LDB"
-    MDB "github.com/IngenieroRicardo/db/MDB"
-    PDB "github.com/IngenieroRicardo/db/PDB"
-    SDB "github.com/IngenieroRicardo/db/SDB"
-    ODB "github.com/IngenieroRicardo/db/ODB"
-    "database/sql"
-    "sync"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	STRC "github.com/IngenieroRicardo/db/STRUCTURES"
+	LDB "github.com/IngenieroRicardo/db/LDB"
+	MDB "github.com/IngenieroRicardo/db/MDB"
+	PDB "github.com/IngenieroRicardo/db/PDB"
+	SDB "github.com/IngenieroRicardo/db/SDB"
+	ODB "github.com/IngenieroRicardo/db/ODB"
+	"database/sql"
+	"sync"
 )
 
 // Connector represents a database connection
 type Connector struct {
     db      *sql.DB
     driver  string
-    mu      sync.Mutex
+    //mu      sync.Mutex
 }
 
 // connectionPool stores active connections
@@ -32,12 +32,25 @@ var connectionPool = struct {
 }
 
 // LoadSQL creates or returns an existing connection
-func LoadSQL(driver string, conexion string) (*Connector, error) {
+//func LoadSQL(driver string, conexion string) (*Connector, error) {
+func LoadSQL(driver string, conexion string, maxOpenConns, maxIdleConns int, connMaxLifetime, connMaxIdleTime time.Duration) (*Connector, error) {
     connectionPool.Lock()
     defer connectionPool.Unlock()
     
     key := driver + ":" + conexion
     if conn, exists := connectionPool.connections[key]; exists {
+        if maxOpenConns > 0 {
+            conn.db.SetMaxOpenConns(maxOpenConns)
+        }
+        if maxIdleConns > 0 {
+            conn.db.SetMaxIdleConns(maxIdleConns)
+        }
+        if connMaxLifetime > 0 {
+            conn.db.SetConnMaxLifetime(connMaxLifetime)
+        }
+        if connMaxIdleTime > 0 {
+            conn.db.SetConnMaxIdleTime(connMaxIdleTime)
+        }
         return conn, nil
     }
     
@@ -60,6 +73,19 @@ func LoadSQL(driver string, conexion string) (*Connector, error) {
     if err != nil {
         return nil, err
     }
+
+    if maxOpenConns > 0 {
+        db.SetMaxOpenConns(maxOpenConns)
+    }
+    if maxIdleConns > 0 {
+        db.SetMaxIdleConns(maxIdleConns)
+    }
+    if connMaxLifetime > 0 {
+        db.SetConnMaxLifetime(connMaxLifetime)
+    }
+    if connMaxIdleTime > 0 {
+        db.SetConnMaxIdleTime(connMaxIdleTime)
+    }
     
     connector := &Connector{
         db:     db,
@@ -72,8 +98,8 @@ func LoadSQL(driver string, conexion string) (*Connector, error) {
 
 // SQLrunonLoad executes a query using a preloaded connection
 func SQLrunonLoad(connector *Connector, query string, args ...string) STRC.InternalResult {
-    connector.mu.Lock()
-    defer connector.mu.Unlock()
+    //connector.mu.Lock()
+    //defer connector.mu.Unlock()
     
     var goArgs []interface{}
     var result STRC.InternalResult
@@ -167,73 +193,73 @@ func CloseSQL(connector *Connector) error {
 
 
 func SQLrun(driver string, conexion string, query string, args ...string) STRC.InternalResult {
-    var result STRC.InternalResult
-    var goArgs []interface{}
+	var result STRC.InternalResult
+	var goArgs []interface{}
 
-    // Procesar cada argumento
-    for _, arg := range args {
-        switch {
-        case strings.HasPrefix(arg, "int::"):
-            intVal, err := strconv.ParseInt(arg[5:], 10, 64)
-            if err != nil {
-                result.Json = createErrorJSON(fmt.Sprintf("Error parseando entero: %s", arg[5:]))
-                result.Is_error = 1
-                return result
-            }
-            goArgs = append(goArgs, intVal)
+	// Procesar cada argumento
+	for _, arg := range args {
+		switch {
+		case strings.HasPrefix(arg, "int::"):
+			intVal, err := strconv.ParseInt(arg[5:], 10, 64)
+			if err != nil {
+				result.Json = createErrorJSON(fmt.Sprintf("Error parseando entero: %s", arg[5:]))
+				result.Is_error = 1
+				return result
+			}
+			goArgs = append(goArgs, intVal)
 
-        case strings.HasPrefix(arg, "float::"), strings.HasPrefix(arg, "double::"):
-            prefixLen := 7
-            if strings.HasPrefix(arg, "double::") {
-                prefixLen = 8
-            }
-            floatVal, err := strconv.ParseFloat(arg[prefixLen:], 64)
-            if err != nil {
-                result.Json = createErrorJSON(fmt.Sprintf("Error parseando float: %s", arg[prefixLen:]))
-                result.Is_error = 1
-                return result
-            }
-            goArgs = append(goArgs, floatVal)
+		case strings.HasPrefix(arg, "float::"), strings.HasPrefix(arg, "double::"):
+			prefixLen := 7
+			if strings.HasPrefix(arg, "double::") {
+				prefixLen = 8
+			}
+			floatVal, err := strconv.ParseFloat(arg[prefixLen:], 64)
+			if err != nil {
+				result.Json = createErrorJSON(fmt.Sprintf("Error parseando float: %s", arg[prefixLen:]))
+				result.Is_error = 1
+				return result
+			}
+			goArgs = append(goArgs, floatVal)
 
-        case strings.HasPrefix(arg, "bool::"):
-            boolVal, err := strconv.ParseBool(arg[6:])
-            if err != nil {
-                result.Json = createErrorJSON(fmt.Sprintf("Error parseando booleano: %s", arg[6:]))
-                result.Is_error = 1
-                return result
-            }
-            goArgs = append(goArgs, boolVal)
+		case strings.HasPrefix(arg, "bool::"):
+			boolVal, err := strconv.ParseBool(arg[6:])
+			if err != nil {
+				result.Json = createErrorJSON(fmt.Sprintf("Error parseando booleano: %s", arg[6:]))
+				result.Is_error = 1
+				return result
+			}
+			goArgs = append(goArgs, boolVal)
 
-        case strings.HasPrefix(arg, "null::"):
-            goArgs = append(goArgs, nil)
+		case strings.HasPrefix(arg, "null::"):
+			goArgs = append(goArgs, nil)
 
-        case strings.HasPrefix(arg, "blob::"):
-            data, err := base64.StdEncoding.DecodeString(arg[6:])
-            if err != nil {
-                result.Json = createErrorJSON(fmt.Sprintf("Error decodificando blob: %v", err))
-                result.Is_error = 1
-                return result
-            }
-            goArgs = append(goArgs, data)
+		case strings.HasPrefix(arg, "blob::"):
+			data, err := base64.StdEncoding.DecodeString(arg[6:])
+			if err != nil {
+				result.Json = createErrorJSON(fmt.Sprintf("Error decodificando blob: %v", err))
+				result.Is_error = 1
+				return result
+			}
+			goArgs = append(goArgs, data)
 
-        default:
-            goArgs = append(goArgs, arg)
-        }
-    }
+		default:
+			goArgs = append(goArgs, arg)
+		}
+	}
 
-    // Ejecutar según el driver
-    switch driver {
-    case "sqlite3":
-        return LDB.SqlRunInternal(driver, conexion, query, goArgs...)
-    case "sqlserver":
-        return SDB.SqlRunInternal(driver, conexion, query, goArgs...)
-    case "postgres":
-        return PDB.SqlRunInternal(driver, conexion, query, goArgs...)
-    case "oracle":
-        return ODB.SqlRunInternal("godror", conexion, query, goArgs...)
-    default:
-        return MDB.SqlRunInternal(driver, conexion, query, goArgs...)
-    }
+	// Ejecutar según el driver
+	switch driver {
+	case "sqlite3":
+		return LDB.SqlRunInternal(driver, conexion, query, goArgs...)
+	case "sqlserver":
+		return SDB.SqlRunInternal(driver, conexion, query, goArgs...)
+	case "postgres":
+		return PDB.SqlRunInternal(driver, conexion, query, goArgs...)
+	case "oracle":
+		return ODB.SqlRunInternal("godror", conexion, query, goArgs...)
+	default:
+		return MDB.SqlRunInternal(driver, conexion, query, goArgs...)
+	}
 }
 
 func createErrorJSON(message string) string {
