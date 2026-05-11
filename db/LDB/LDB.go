7d4b53df636a2a01dd035c83bb34504d01bfd68c
WebPrivada <transportresargote@gmail.com>
2026-05-11 13:04:08 -0600

    Update LDB.go

--- db/LDB/LDB.go
+++ db/LDB/LDB.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"bytes"
 	_ "github.com/mattn/go-sqlite3"
-	STRC "github.com/IngenieroRicardo/db/STRUCTURES"
+	STRC "github.com/WebPrivada/SDK/db/STRUCTURES"
 	"regexp"
 	"errors"
 )
